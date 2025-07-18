29653cfc3ab43ac8bc2b5daea9d9182b2bd8e6c2
bigbabyboost <ah0199004@gmail.com>
2025-07-18 10:07:15 +0700

    greetd

--- system/services/greetd.nix
+++ system/services/greetd.nix
@@ -1,8 +1,12 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   # greetd display manager
   services.greetd = let
     session = {
-      command = "$(lib.getExe config.programs.uwsm.package) start hyprland-uwsm.desktop";
+      command = "${lib.getExe config.programs.uwsm.package} start hyprland-uwsm.desktop";
       user = "xaolan";
     };
   in {
