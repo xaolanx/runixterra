801bb5b3081c0e51ba0a38a2ebf60664060aaf8c
bigbabyboost <ah0199004@gmail.com>
2025-07-21 06:12:15 +0700

    my machine changes

--- nixosModules/graphics/intel.nix
+++ nixosModules/graphics/intel.nix
@@ -16,6 +16,7 @@ in {
   config = mkIf (cfg.enable && config.ionia.graphics.enable) {
     hardware.graphics.extraPackages = [
       cfg.intelQSVprovider
+      pkgs.intel-media-sdk
       pkgs.intel-vaapi-driver
       pkgs.libvdpau-va-gl
       pkgs.intel-ocl
