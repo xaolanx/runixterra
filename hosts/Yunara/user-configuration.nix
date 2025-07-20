64e92cf580a3dd5a1bd4b4b984e4249e8c81930f
bigbabyboost <ah0199004@gmail.com>
2025-07-20 14:49:49 +0700

    override brave flags

--- hosts/Yunara/user-configuration.nix
+++ hosts/Yunara/user-configuration.nix
@@ -4,6 +4,22 @@
   config,
   ...
 }: let
+  braveWithFlags = pkgs.brave.override {
+    commandLineArgs = [
+      "--enable-features=AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,CanvasOopRasterization,DefaultANGLEVulkan,EnableDrDc,SkiaGraphite,Vulkan,VulkanFromANGLE,PlatformHEVCDecoderSupport,UseMultiPlaneFormatForHardwareVideo,UseOzonePlatform,VaapiIgnoreDriverChecks"
+      "--flag-switches-end"
+      "--ozone-platform=wayland"
+      "--disable-features=UseChromeOSDirectVideoDecoder"
+      "--ignore-gpu-blocklist"
+      "--enable-zero-copy"
+      "--disable-gpu-driver-bug-workarounds"
+      "--flag-switches-begin"
+      "--enable-gpu-rasterization"
+      "--enable-unsafe-webgpu"
+      "--ozone-platform-hint=wayland"
+    ];
+  };
+
   packages = [
     pkgs.wineWowPackages.stable
     pkgs.bottles
@@ -13,7 +29,7 @@
     pkgs.micro
     pkgs.ripgrep
     pkgs.telegram-desktop
-    pkgs.brave
+    braveWithFlags
 
     # from internal overlay
     pkgs.mpv-wrapped
@@ -46,11 +62,9 @@ in {
         name = "cropped-${image.name}";
         src = image;
         dontUnpack = true;
-        # nativeBuildInputs = [pkgs.scripts.wallcrop];
         nativeBuildInputs = [pkgs.imagemagick];
         installPhase = ''
           magick $src -crop 1920x1080+600+1200 - > $out
-          # wallcrop $src 0 1030 > $out
         '';
       };
   };
@@ -69,6 +83,7 @@ in {
       };
     in
       lib.mkForce face;
+
     "Pictures/booru".source = config.programs.booru-flake.imageFolder;
   };
 }
