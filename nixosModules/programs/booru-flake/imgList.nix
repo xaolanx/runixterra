226fa90e5d1bce9311d9b7ae9b9db95adebd1f60
bigbabyboost <ah0199004@gmail.com>
2025-07-20 09:49:46 +0700

    fix hash

--- nixosModules/programs/booru-flake/imgList.nix
+++ nixosModules/programs/booru-flake/imgList.nix
@@ -5,11 +5,6 @@
     imgHash = "sha256-8OOpj0ltxCwFgILrsj1/E0HJC/v39OqV8+k+Bzb1ji8=";
   }
   {
-    id = "6079266";
-    jsonHash = "sha256-aU/wz9dFE2zr4aeAhGpFjPlAz8Osx7XDYQD17zoBxN0=";
-    imgHash = "sha256-FuxMf6flg4TgkeN4NskZQKyCAYKmWTrSu009loXxyiw=";
-  }
-  {
     id = "6887138";
     jsonHash = "sha256-DZ4AOKqhcwRvMPQPIa2mhsTI2Yq+BA1lSHF6wYcthiQ=";
     imgHash = "sha256-RD7NBsmmdXniAX81uZNAzEZvw/zwKIzb9D20SixNxRs=";
