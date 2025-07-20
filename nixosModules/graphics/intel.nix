Rexcrazy804/Zaphkiel

nixosModules: improved intel module - grant btop capsys perms for intel gpu data - make QSVprovider option

nixosModules/graphics/intel.nix
@@ -3,18 +3,31 @@
   lib,
   config,
   ...
-}: {
-  options.zaphkiel.graphics.intel.enable = lib.mkEnableOption "intel graphics";
-  config = lib.mkIf (config.zaphkiel.graphics.intel.enable && config.zaphkiel.graphics.enable) {
-    # WARN too lazy to futher modularize this maybe re use nixos-hardware's module
-    hardware.graphics.extraPackages = with pkgs; [
-      intel-media-driver
-      vpl-gpu-rt
-      intel-vaapi-driver
-      libvdpau-va-gl
-      intel-ocl
+}: let
+  inherit (lib) mkEnableOption mkIf mkPackageOption getExe;
+  cfg = config.zaphkiel.graphics.intel;
+in {
+  options.zaphkiel.graphics.intel = {
+    enable = mkEnableOption "intel graphics";
+    intelQSVprovider = mkPackageOption pkgs "QSV provider" {
+      default = "intel-media-driver";
+    };
+  };
+  config = mkIf (cfg.enable && config.zaphkiel.graphics.enable) {
+    hardware.graphics.extraPackages = [
+      cfg.intelQSVprovider
+      pkgs.intel-vaapi-driver
+      pkgs.libvdpau-va-gl
+      pkgs.intel-ocl
     ];
     # environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Force intel-media-driver
     # environment.sessionVariables = {LIBVA_DRIVER_NAME = "i965";}; # Force intel-vaapi-driver
+
+    security.wrappers.btop = {
+      owner = "root";
+      group = "root";
+      source = getExe pkgs.btop;
+      capabilities = "cap_perfmon+ep";
+    };
   };
 }

xaolanx/runixterra

nixosModules/graphics/intel.nix
@@ -3,20 +3,37 @@
   lib,
   config,
   ...
-}: {
-  options.ionia.graphics.intel.enable = lib.mkEnableOption "intel graphics";
-  config = lib.mkIf (config.ionia.graphics.intel.enable && config.ionia.graphics.enable) {
-    # WARN too lazy to futher modularize this maybe re use nixos-hardware's module
-    hardware.graphics.extraPackages = with pkgs; [
-      libva
-      intel-media-driver
-      intel-compute-runtime-legacy1
+}: let
+  inherit (lib) mkEnableOption mkIf mkPackageOption getExe;
+  cfg = config.ionia.graphics.intel;
+in {
+  options.ionia.graphics.intel = {
+    enable = mkEnableOption "intel graphics";
+    intelQSVprovider = mkPackageOption pkgs "QSV provider" {
+      default = "intel-media-driver";
+    };
+  };
+  config = mkIf (cfg.enable && config.ionia.graphics.enable) {
+    hardware.graphics.extraPackages = [
+      cfg.intelQSVprovider
+      pkgs.intel-vaapi-driver
+      pkgs.libvdpau-va-gl
+      pkgs.intel-ocl
+      pkgs.intel-compute-runtime-legacy1
+      pkgs.libva
     ];
     environment.sessionVariables = {
       LIBVA_DRIVER_NAME = "iHD";
       LIBVA_DRIVERS_PATH = "${pkgs.intel-media-driver}/lib/dri";
       VKD_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd/x86_64.json";
       OCL_ICD_VENDORS = "${pkgs.intel-compute-runtime-legacy1}/etc/OpenCL/vendors";
+    };
+
+    security.wrappers.btop = {
+      owner = "root";
+      group = "root";
+      source = getExe pkgs.btop;
+      capabilities = "cap_perfmon+ep";
     };
   };
 }
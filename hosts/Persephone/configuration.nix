9d7fc5a640bbba86fbd91510886d9193757eab23
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-07-20 23:40:10 +0400

    nixosModules: improved intel module - grant btop capsys perms for intel gpu data - make QSVprovider option

---
+++ hosts/Persephone/configuration.nix
@@ -0,0 +1,104 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib) mkForce optional;
+in {
+  imports = [
+    ./hardware-configuration.nix
+    ./user-configuration.nix
+    ./extras/privoxy.nix
+    ./extras/lanzaboote.nix
+  ];
+
+  system.stateVersion = "24.11";
+  networking.hostName = "Persephone";
+  time.timeZone = "Asia/Dubai";
+
+  zaphkiel = {
+    graphics = {
+      enable = true;
+      intel = {
+        enable = true;
+        intelQSVprovider = pkgs.vpl-gpu-rt;
+      };
+    };
+
+    programs = {
+      sddm-custom-theme.enable = true;
+      obs-studio.enable = false;
+      steam.enable = false;
+      hyprland.enable = true;
+      keyd.enable = true;
+      firefox.enable = true;
+    };
+
+    services = {
+      enable = true;
+      tailscale = {
+        enable = true;
+        exitNode.enable = false;
+      };
+      openssh.enable = true;
+    };
+  };
+
+  # tailscale
+  age.secrets.tailAuth.file = ../../secrets/secret9.age;
+  services.tailscale.authKeyFile = config.age.secrets.tailAuth.path;
+
+  # forward dns onto the tailnet
+  networking.firewall.allowedTCPPorts = [53];
+  networking.firewall.allowedUDPPorts = [53];
+  services.dnscrypt-proxy2.settings = {
+    listen_addresses = [
+      "100.110.70.18:53"
+      "[fd7a:115c:a1e0::6a01:4614]:53"
+      "127.0.0.1:53"
+      "[::1]:53"
+    ];
+  };
+
+  # generic
+  programs = {
+    kdeconnect = {
+      enable = true;
+      package = pkgs.kdePackages.kdeconnect-kde;
+    };
+  };
+
+  hardware.bluetooth.powerOnBoot = mkForce false;
+
+  # finger print
+  systemd.services.fprintd = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig.Type = "simple";
+  };
+  services.fprintd = {
+    enable = true;
+    tod.enable = true;
+    tod.driver = pkgs.libfprint-2-tod1-elan;
+  };
+
+  virtualisation.podman = {
+    enable = true;
+    defaultNetwork.settings.dns_enabled = true;
+  };
+  users.users.rexies.extraGroups = ["podman"];
+
+  # disable network manager wait online service (+6 seconds to boot time!!!!)
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  services.sunshine = {
+    enable = true;
+    autoStart = false;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+
+  # idk the service didn't show up and now it does too lazy to rebuild and test
+  # if it was a delusion. If it works don't break it, amiright
+  systemd.user.services.sunshine.wantedBy = mkForce (optional config.services.sunshine.autoStart "graphical-session.target");
+}
--- nixosModules/graphics/intel.nix
+++ nixosModules/graphics/intel.nix
@@ -3,20 +3,37 @@
   lib,
   config,
   ...
-}: {
-  options.ionia.graphics.intel.enable = lib.mkEnableOption "intel graphics";
-  config = lib.mkIf (config.ionia.graphics.intel.enable && config.ionia.graphics.enable) {
-    # WARN too lazy to futher modularize this maybe re use nixos-hardware's module
-    hardware.graphics.extraPackages = with pkgs; [
-      libva
-      intel-media-driver
-      intel-compute-runtime-legacy1
+}: let
+  inherit (lib) mkEnableOption mkIf mkPackageOption getExe;
+  cfg = config.ionia.graphics.intel;
+in {
+  options.ionia.graphics.intel = {
+    enable = mkEnableOption "intel graphics";
+    intelQSVprovider = mkPackageOption pkgs "QSV provider" {
+      default = "intel-media-driver";
+    };
+  };
+  config = mkIf (cfg.enable && config.ionia.graphics.enable) {
+    hardware.graphics.extraPackages = [
+      cfg.intelQSVprovider
+      pkgs.intel-vaapi-driver
+      pkgs.libvdpau-va-gl
+      pkgs.intel-ocl
+      pkgs.intel-compute-runtime-legacy1
+      pkgs.libva
     ];
     environment.sessionVariables = {
       LIBVA_DRIVER_NAME = "iHD";
       LIBVA_DRIVERS_PATH = "${pkgs.intel-media-driver}/lib/dri";
       VKD_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd/x86_64.json";
       OCL_ICD_VENDORS = "${pkgs.intel-compute-runtime-legacy1}/etc/OpenCL/vendors";
+    };
+
+    security.wrappers.btop = {
+      owner = "root";
+      group = "root";
+      source = getExe pkgs.btop;
+      capabilities = "cap_perfmon+ep";
     };
   };
 }
