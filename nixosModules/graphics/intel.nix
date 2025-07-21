e183085a5859c22568e8874ba9312f466ce54a46
bigbabyboost <ah0199004@gmail.com>
2025-07-22 04:30:07 +0700

    fix intel

--- nixosModules/graphics/intel.nix
+++ nixosModules/graphics/intel.nix
@@ -12,7 +12,6 @@ in {
   };
   config = mkIf (cfg.enable && config.ionia.graphics.enable) {
     hardware.graphics.extraPackages = [
-      cfg.intelQSVprovider
       pkgs.intel-vaapi-driver
       pkgs.libvdpau-va-gl
       pkgs.intel-ocl
