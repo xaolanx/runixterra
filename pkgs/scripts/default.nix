5050abbe4f3121192faf8f34a127ca2651380fde
xaolan <ah0199004@gmail.com>
2025-07-23 05:06:39 +0700

    try to fix

--- pkgs/scripts/default.nix
+++ pkgs/scripts/default.nix
@@ -2,12 +2,12 @@
   lib,
   callPackage,
 }:
-lib.makeExtensible (_final: {
+lib.makeExtensible (final: {
   wallcrop = callPackage ./wallcrop.nix {};
   cowask = callPackage ./cowask.nix {};
-  gpurecording = callPackage ./gpurecording.nix {};
-  kde-send = callPackage ./kde-send.nix {};
   writeAwk = callPackage ./writeAwkScript.nix {};
   writeAwkBin = name: final.writeAwkScript "/bin/${name}";
+  gpurecording = callPackage ./gpurecording.nix {};
+  kde-send = callPackage ./kde-send.nix {};
   npins-show = callPackage ./npins-show.nix {inherit (final) writeAwk;};
 })
