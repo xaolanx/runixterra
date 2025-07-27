54f2f20306f584734ac624ce6952c864e4c38116
Xaolan <ah0199004@gmail.com>
2025-07-28 07:20:28 +0800

    refactor(security): wheel group sudo nopasswd

--- config/optional/core/security.nix
+++ config/optional/core/security.nix
@@ -2,23 +2,13 @@
   security = {
     polkit = {
       enable = true;
-      extraConfig = ''
-        polkit.addRule(function(action, subject) {
-          if (
-            subject.isInGroup("users")
-              && (
-                action.id == "org.freedesktop.login1.reboot" ||
-                action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-                action.id == "org.freedesktop.login1.power-off" ||
-                action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-              )
-            )
-          {
-            return polkit.Result.YES;
-          }
-        });
-      '';
     };
+
+    # userland niceness
+    rtkit.enable = true;
+
+    # don't ask for password for wheel group
+    sudo.wheelNeedsPassword = false;
   };
 
   systemd.user.services.polkit-kde-authentication-agent-1 = {
