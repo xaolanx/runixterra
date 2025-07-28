ef25df690eb0a7ec50efc95feeede882631a1c27
Xaolan <ah0199004@gmail.com>
2025-07-28 19:58:59 +0800

    feat(bluetooth): add overskride

--- config/optional/hardware/bluetooth.nix
+++ config/optional/hardware/bluetooth.nix
@@ -2,7 +2,22 @@
   hardware.bluetooth = {
     enable = true;
     package = pkgs.bluez5-experimental;
+    settings = {
+      # make Xbox Series X controller work
+      General = {
+        Class = "0x000100";
+        ControllerMode = "bredr";
+        FastConnectable = true;
+        JustWorksRepairing = "always";
+        Privacy = "device";
+        # Battery info for Bluetooth devices
+        Experimental = true;
+      };
+    };
   };
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
+
+  hj.packages = [pkgs.overskride];
+
+  # https://github.com/NixOS/nixpkgs/issues/114222
+  systemd.user.services.telephony_client.enable = false;
 }
