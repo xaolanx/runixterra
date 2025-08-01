0b2a09b8ed6d33c73c720cb290a50e5f131f0d78
Xaolan <ah0199004@gmail.com>
2025-08-01 14:22:13 +0800

    test: ims

--- config/base/nix.nix
+++ config/base/nix.nix
@@ -44,9 +44,13 @@ in {
   };
 
   nixpkgs.overlays = [
-    (import ./overlays/intel-media-sdk-override.nix)
+    (_self: super: {
+      intel-media-sdk = super.intel-media-sdk.overrideAttrs (old: {
+        cmakeFlags = old.cmakeFlags ++ ["-DCMAKE_CXX_STANDARD=17"];
+        NIX_CFLAGS_COMPILE = "-std=c++17";
+      });
+    })
   ];
-
   nixpkgs = {
     config = {
       allowUnfree = true;
