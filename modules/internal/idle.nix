6c30de2780b7fa9ade00068bac352699d60f2e38
Xaolan <ah0199004@gmail.com>
2025-08-02 06:24:05 +0800

    chore: hypridle

--- modules/internal/idle.nix
+++ modules/internal/idle.nix
@@ -3,32 +3,41 @@
   lib,
   myLib,
   inputs',
+  self',
+  pkgs,
   ...
 }: let
   inherit (lib) mkIf mkOption types;
   inherit (myLib.generators) toHyprConf;
   inherit (inputs'.hypridle.packages) hypridle;
+  inherit (lib.meta) getExe getExe';
+
+  noctalia = getExe self'.packages.noctalia;
+  loginctl = getExe' pkgs.systemd "loginctl";
+  systemctl = getExe' pkgs.systemd "systemctl";
+  hyprctl = getExe' config.programs.hyprland.package "hyprctl";
+  niri = getExe config.programs.niri.package;
 
   # Shared listener definitions
   commonListeners = [
     {
       timeout = 300;
-      on-timeout = "loginctl lock-session";
+      on-timeout = "${loginctl} lock-session";
     }
     {
       timeout = 330;
       on-timeout =
         if config.programs.niri.enable
-        then "niri msg action power-off-monitors"
-        else "hyprctl dispatch dpms off";
+        then "${niri} msg action power-off-monitors"
+        else "${hyprctl} dispatch dpms off";
       on-resume =
         if config.programs.niri.enable
-        then "niri msg action power-on-monitors"
-        else "hyprctl dispatch dpms on";
+        then "${niri} msg action power-on-monitors"
+        else "${hyprctl} dispatch dpms on";
     }
     {
       timeout = 500;
-      on-timeout = "systemctl suspend";
+      on-timeout = "${systemctl} suspend";
     }
   ];
 
@@ -36,14 +45,14 @@
   generalSettings =
     if config.programs.niri.enable
     then {
-      lock_cmd = "noctalia ipc call globalIPC toggleLock";
-      before_sleep_cmd = "loginctl lock-session";
-      after_sleep_cmd = "niri msg action power-on-monitors";
+      lock_cmd = "${noctalia} ipc call globalIPC toggleLock";
+      before_sleep_cmd = "${loginctl} lock-session";
+      after_sleep_cmd = "${niri} msg action power-on-monitors";
     }
     else {
-      lock_cmd = "noctalia ipc call globalIPC toggleLock";
-      before_sleep_cmd = "loginctl lock-session";
-      after_sleep_cmd = "hyprctl dispatch dpms on";
+      lock_cmd = "${noctalia} ipc call globalIPC toggleLock";
+      before_sleep_cmd = "${loginctl} lock-session";
+      after_sleep_cmd = "${hyprctl} dispatch dpms on";
     };
 
   # Final Hypridle config
