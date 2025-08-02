f5aa46d792ede84ef76b59331b11797336ab29e3
Xaolan <49628247+xaolanx@users.noreply.github.com>
2025-08-02 18:31:09 +0800

    refactor(myLib): change toConf to toMpvConf (#27)

---
+++ lib/generators/toMpvConf.nix
@@ -0,0 +1,26 @@
+{lib}: attrs: let
+  mkValueString = v:
+    if lib.isList v
+    then lib.concatStringsSep "," v
+    else if lib.isBool v
+    then
+      if v
+      then "yes"
+      else "no"
+    else toString v;
+
+  regular = lib.filterAttrs (k: _: !lib.hasPrefix "__section_" k) attrs;
+  sections = lib.filterAttrs (k: _: lib.hasPrefix "__section_" k) attrs;
+
+  baseConfig = lib.generators.toKeyValue {
+    mkKeyValue = lib.generators.mkKeyValueDefault {
+      inherit mkValueString;
+    } "=";
+  };
+in
+  lib.concatStringsSep "\n" (
+    [(baseConfig regular)]
+    ++ (lib.mapAttrsToList
+      (k: v: "\n[${lib.removePrefix "__section_" k}]\n${baseConfig v}")
+      sections)
+  )
