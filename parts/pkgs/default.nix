6b433eb8b732926b4f1ee66b41b412690f41efc3
Xaolan <ah0199004@gmail.com>
2025-08-31 22:19:45 +0700

    test: pins shell

--- parts/pkgs/default.nix
+++ parts/pkgs/default.nix
@@ -6,43 +6,43 @@ _: {
     pins,
     ...
   }: let
-    cli = self'.packages.caelestia-cli;
-    shell = self'.packages.caelestia-shell;
     app2unitPkg = self'.packages.app2unit;
 
     basePackages = lib.packagesFromDirectoryRecursive {
       inherit (pkgs) callPackage;
       directory = ./packages;
     };
+
+    cliAndShell = lib.fix (self: {
+      caelestia-cli = pkgs.callPackage (pins.caelestia-cli + "/default.nix") {
+        rev = pins.caelestia-cli.rev or "dirty";
+        caelestia-shell = pkgs.runCommand "dummy-shell" {} "mkdir -p $out";
+        app2unit = app2unitPkg;
+        withShell = false;
+      };
+
+      caelestia-shell = pkgs.callPackage (pins.caelestia-shell + "/nix/default.nix") {
+        rev = pins.caelestia-shell.rev or "dirty";
+        caelestia-cli = self.caelestia-cli;
+        quickshell = pkgs.quickshell;
+        app2unit = app2unitPkg;
+        withCli = true;
+      };
+    });
   in {
     packages =
       basePackages
       // {
-        # kurukurubar = basePackages.kurukurubar.override {
-        #   configPath = ./../../assets/kurukurubar;
-        #   quickshell = quickshellPkg;
-        # };
-
         noctalia = basePackages.noctalia.override {
           configPath = ./../../assets/noctalia;
         };
+
         kvlibadwaita = basePackages.kvlibadwaita.override {
           src = pins.kvlibadwaita;
           theme = "woodland";
         };
-        caelestia-shell = basePackages.caelestia-shell.override {
-          caelestia-cli = cli;
-          quickshell = pkgs.quickshell;
-          app2unit = app2unitPkg;
-          withCli = false;
-          src = pins.caelestia-shell;
-        };
-        caelestia-cli = basePackages.caelestia-cli.override {
-          caelestia-shell = shell;
-          app2unit = app2unitPkg;
-          withShell = true;
-          src = pins.caelestia-cli;
-        };
+
+        inherit (cliAndShell) caelestia-shell caelestia-cli;
       };
   };
 }
