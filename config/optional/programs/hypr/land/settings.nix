340c26cef7263908fda44aff7e209f3ee94ca7fa
Xaolan <ah0199004@gmail.com>
2025-09-01 07:43:14 +0700

    chore: hypland gesture

--- config/optional/programs/hypr/land/settings.nix
+++ config/optional/programs/hypr/land/settings.nix
@@ -237,11 +237,9 @@ in {
             };
           };
 
-          gestures = {
-            workspace_swipe = true;
-            workspace_swipe_direction_lock = false;
-            workspace_swipe_cancel_ratio = 0.15;
-          };
+          gesture = [
+            "3, horizontal, workspace"
+          ];
 
           misc = {
             force_default_wallpaper = 0;
