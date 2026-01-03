45cde325920c90c39869e74080901596be30aa13
Xaolan <49628247+xaolanx@users.noreply.github.com>
2026-01-04 01:21:31 +0700

    tes oin

--- config/base/nix.nix
+++ config/base/nix.nix
@@ -37,14 +37,17 @@
         NIX_CFLAGS_COMPILE = "-std=c++17";
       });
 
-      npins = super.npins.overrideAttrs (_: {
+      npins = super.npins.overrideAttrs (_old: rec {
+        pname = "npins";
         version = "0.3.1";
+
         src = super.fetchFromGitHub {
           owner = "andir";
           repo = "npins";
-          tag = "0.3.1";
+          rev = "${version}";
           sha256 = "sha256-PPk9Ve1pM3X7NfGeGb8Jiq4YDEwAjErP4xzGwLaakTU=";
         };
+
         cargoHash = "sha256-YRW2TqbctuGC2M6euR4bb0m9a19m8WQVvWucRMpzkQE=";
       });
     })
