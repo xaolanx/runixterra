df1c4a07ebed4d24c82fa8a92aed538cb70a3c8f
Xaolan <49628247+xaolanx@users.noreply.github.com>
2026-01-04 07:28:24 +0700

    runtime legacy

--- hosts/yunara/config/intel.nix
+++ hosts/yunara/config/intel.nix
@@ -4,7 +4,7 @@
     pkgs.intel-media-driver
     pkgs.libvdpau-va-gl
     pkgs.intel-ocl
-    pkgs.intel-compute-runtime-legacy1
+   # pkgs.intel-compute-runtime-legacy1
     pkgs.libva
     pkgs.vulkan-tools
     pkgs.intel-media-sdk
@@ -13,7 +13,7 @@
     LIBVA_DRIVER_NAME = "iHD";
     LIBVA_DRIVERS_PATH = "${pkgs.intel-media-driver}/lib/dri";
     VKD_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd/x86_64.json";
-    OCL_ICD_VENDORS = "${pkgs.intel-compute-runtime-legacy1}/etc/OpenCL/vendors";
+   # OCL_ICD_VENDORS = "${pkgs.intel-compute-runtime-legacy1}/etc/OpenCL/vendors";
     ANV_DEBUG = "video-decode";
     ANV_VIDEO_DECODE = "1";
   };
